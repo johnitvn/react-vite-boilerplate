edd4f246e0cada57de7213651f94bd242762be36
John Martin <john.itvn@gmail.com>
2023-02-04 16:16:58 +0700

    feat: add tailwind support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
@@ -10,5 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-  }
+    include: ['src/**/*.test.{ts,tsx}'],
+    exclude: ['**/node_modules/**', '**/dist/**', 'e2e/**'],
+  },
 })
