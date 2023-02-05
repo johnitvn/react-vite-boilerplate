17d61c21c8715e627c4e45164fa3a4c22bace672
John Martin <john.itvn@gmail.com>
2023-02-05 14:01:04 +0700

    chore: reset base

--- src/main.tsx
+++ src/main.tsx
@@ -1,62 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Route, Routes } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
-import './main.css'
+import './assets/styles/app.css'
 
-const LandingLayout = React.lazy(() => import('./layouts/LandingLayout'))
-const Index = React.lazy(() => import('./views/Index'))
-const SignIn = React.lazy(() => import('./views/Authentication/SignIn'))
-const SignUp = React.lazy(() => import('./views/Authentication/SignUp'))
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route
-        path="/"
-        element={
-          <React.Suspense fallback={<>...</>}>
-            <LandingLayout />
-          </React.Suspense>
-        }
-      >
-        <Route
-          path=""
-          index
-          element={
-            <React.Suspense fallback={<>...</>}>
-              <Index />
-            </React.Suspense>
-          }
-        />
-        <Route
-          path="signin"
-          index
-          element={
-            <React.Suspense fallback={<>...</>}>
-              <SignIn />
-            </React.Suspense>
-          }
-        />
-        <Route
-          path="signup"
-          index
-          element={
-            <React.Suspense fallback={<>...</>}>
-              <SignUp />
-            </React.Suspense>
-          }
-        />
+      <Route path="/">
+        <Route path="" index />
       </Route>
-      <Route
-        path="*"
-        index
-        element={
-          <React.Suspense fallback={<>...</>}>
-            <Index />
-          </React.Suspense>
-        }
-      />
     </Routes>
   </BrowserRouter>,
 )
